b2e2f511fc04d9e3c62b58b6024704d6eeafd233
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-27 14:07:57 -0400

    added sequelize information to server.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Changed DB_PW to DB_PASSWORD in order to have variables the same as in the .env file
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
